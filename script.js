0ffae3ede6043be4f218d2a98d7a2c98b4e577a4
SIDDHARTH SINGH <singhsiddharth5050@gmail.com>
2023-12-09 18:14:26 +0530

    Not an final commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const apikey = "dd94f859a0e52d6e4767fddf735f04a7"
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon  = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`)
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
+
+// console.log(data);       /   
+
+document.querySelector(".city").innerHTML = data.name;
+document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    if (data.weather[0].main=="Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    }else if(data.weather[0].main=="Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src = "images/rain.png";
+    }else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }else if(data.weather[0].main=="Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";  
+    document.querySelector(".error").style.display = "none";
+    }
+
+
+    }
+searchbtn.addEventListener("click", () => {
+    checkweather(searchbox.value);
+});
